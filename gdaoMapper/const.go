6f9a7f0984dcc5dda43a9656cd32245a3e7a513c
donnie4w <donnie4w@gmail.com>
2024-07-24 01:33:41 +0800

    Create const.go

---
+++ gdaoMapper/const.go
@@ -0,0 +1,17 @@
+// Copyright (c) 2024, donnie <donnie4w@gmail.com>
+// All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+//
+// github.com/donnie4w/gdao
+
+package gdaoMapper
+
+type sqlType int8
+
+const (
+	_SELECT sqlType = 1
+	_UPDATE sqlType = 2
+	_INSERT sqlType = 3
+	_DELETE sqlType = 4
+)
