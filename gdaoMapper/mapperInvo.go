aa7eb810f8c3d1fb655cf753776b6d4a4e11eee3
donnie4w <donnie4w@gmail.com>
2024-08-14 11:00:07 +0800

    Update mapperInvo.go

--- gdaoMapper/mapperInvo.go
+++ gdaoMapper/mapperInvo.go
@@ -9,7 +9,6 @@ package gdaoMapper
 
 import (
 	"fmt"
-	"github.com/donnie4w/gdao"
 	"github.com/donnie4w/gdao/base"
 	"github.com/donnie4w/gdao/gdaoCache"
 	"github.com/donnie4w/gdao/util"
@@ -19,18 +18,18 @@ import (
 
 type mapperInvoke[T any] mapperHandler
 
-func (m *mapperInvoke[T]) Select(mapperId string, args ...any) (r *T, er error) {
+func (m *mapperInvoke[T]) SelectDirect(mapperId string, args ...any) (r *T, er error) {
 	if pb, ok := mapperparser.getParamBean(mapperId); !ok {
 		return nil, fmt.Errorf("Mapper Id not found [%s]", mapperId)
 	} else {
 		if base.Logger.IsVaild {
-			base.Logger.Debug("[Mapper Id] "+mapperId+" \nSelect SQL["+pb.sql+"]ARGS", args)
+			base.Logger.Debug("[Mapper Id] "+mapperId+" \nSelectDirect SQL["+pb.sql+"]ARGS", args)
 		}
 		return _select[T]((*mapperHandler)(m), pb, args...)
 	}
 }
 
-func (m *mapperInvoke[T]) SelectAny(mapperId string, parameter any) (r *T, err error) {
+func (m *mapperInvoke[T]) Select(mapperId string, parameter any) (r *T, err error) {
 	var pb *paramBean
 	var args []any
 	mh := (*mapperHandler)(m)
@@ -38,7 +37,7 @@ func (m *mapperInvoke[T]) SelectAny(mapperId string, parameter any) (r *T, err e
 		return r, err
 	}
 	if base.Logger.IsVaild {
-		base.Logger.Debug("[Mapper Id] "+mapperId+" \nSelectAny SQL["+pb.sql+"]ARGS", args)
+		base.Logger.Debug("[Mapper Id] "+mapperId+" \nSelect SQL["+pb.sql+"]ARGS", args)
 	}
 	return _select[T](mh, pb, args...)
 }
@@ -57,15 +56,20 @@ func _select[T any](mh *mapperHandler, pb *paramBean, args ...any) (r *T, err er
 		}
 	}
 	var databean *base.DataBean
-	if databean, err = mh.getDBhandle(pb.namespace, pb.id, true).ExecuteQueryBean(pb.sql, args...); err == nil {
+	if databean = mh.getDBhandle(pb.namespace, pb.id, true).ExecuteQueryBean(pb.sql, args...); databean.GetError() == nil {
 		if isDBType(pb.outputType) {
 			r, err = toT[T](databean)
 		}
 		if r == nil {
-			if r, err = gdao.Scan[T](databean); err != nil || r == nil {
-				r, err = toT[T](databean)
+			if err = databean.GetError(); err == nil {
+				r = new(T)
+				if err = databean.Scan(r); err != nil {
+					r, err = toT[T](databean)
+				}
 			}
 		}
+	} else {
+		err = databean.GetError()
 	}
 
 	if isCache && r != nil && err == nil {
@@ -77,18 +81,18 @@ func _select[T any](mh *mapperHandler, pb *paramBean, args ...any) (r *T, err er
 	return
 }
 
-func (m *mapperInvoke[T]) Selects(mapperId string, args ...any) (r []*T, er error) {
+func (m *mapperInvoke[T]) SelectsDirect(mapperId string, args ...any) (r []*T, er error) {
 	if pb, ok := mapperparser.getParamBean(mapperId); !ok {
 		return nil, fmt.Errorf("Mapper Id not found [%s]", mapperId)
 	} else {
 		if base.Logger.IsVaild {
-			base.Logger.Debug("[Mapper Id] "+mapperId+" \nSelects SQL["+pb.sql+"]ARGS", args)
+			base.Logger.Debug("[Mapper Id] "+mapperId+" \nSelectsDirect SQL["+pb.sql+"]ARGS", args)
 		}
 		return selects[T]((*mapperHandler)(m), pb, args...)
 	}
 }
 
-func (m *mapperInvoke[T]) SelectsAny(mapperId string, parameter any) (r []*T, err error) {
+func (m *mapperInvoke[T]) Selects(mapperId string, parameter any) (r []*T, err error) {
 	var pb *paramBean
 	var args []any
 	mh := (*mapperHandler)(m)
@@ -96,7 +100,7 @@ func (m *mapperInvoke[T]) SelectsAny(mapperId string, parameter any) (r []*T, er
 		return r, err
 	}
 	if base.Logger.IsVaild {
-		base.Logger.Debug("[Mapper Id] "+mapperId+" \nSelectsAny SQL["+pb.sql+"]ARGS", args)
+		base.Logger.Debug("[Mapper Id] "+mapperId+" \nSelects SQL["+pb.sql+"]ARGS", args)
 	}
 	return selects[T](mh, pb, args...)
 }
@@ -114,22 +118,19 @@ func selects[T any](mh *mapperHandler, pb *paramBean, args ...any) (r []*T, err 
 			return result.([]*T), nil
 		}
 	}
-	var databeans []*base.DataBean
-	if databeans, err = mh.getDBhandle(pb.namespace, pb.id, true).ExecuteQueryBeans(pb.sql, args...); err == nil {
-		if err != nil || databeans == nil {
-			return nil, err
-		}
+	if databeans := mh.getDBhandle(pb.namespace, pb.id, true).ExecuteQueryBeans(pb.sql, args...); databeans.GetError() == nil && databeans.Len() > 0 {
 		r = make([]*T, 0)
 		if isDBType(pb.outputType) {
-			for _, databean := range databeans {
+			for _, databean := range databeans.Beans {
 				if v, _ := toT[T](databean); v != nil {
 					r = append(r, v)
 				}
 			}
 		} else {
 			ok := true
-			for _, databean := range databeans {
-				if v, err := gdao.Scan[T](databean); err == nil && v != nil {
+			for _, databean := range databeans.Beans {
+				v := new(T)
+				if err := databean.Scan(v); err == nil {
 					r = append(r, v)
 				} else {
 					ok = false
@@ -140,7 +141,7 @@ func selects[T any](mh *mapperHandler, pb *paramBean, args ...any) (r []*T, err 
 				if len(r) > 0 {
 					r = make([]*T, 0)
 				}
-				for _, databean := range databeans {
+				for _, databean := range databeans.Beans {
 					if v, _ := toT[T](databean); v != nil {
 						r = append(r, v)
 					}
@@ -153,6 +154,8 @@ func selects[T any](mh *mapperHandler, pb *paramBean, args ...any) (r []*T, err 
 				base.Logger.Debug("[SET CACHE]["+pb.sql+"]", args)
 			}
 		}
+	} else {
+		err = databeans.GetError()
 	}
 	return
 }
