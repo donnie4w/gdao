72e154594b9fb9769c123b01e83dba7db1f675d0
donnie4w <donnie4w@gmail.com>
2024-08-20 00:33:07 +0800

    Update util.go

--- util/util.go
+++ util/util.go
@@ -20,34 +20,36 @@ func ToUpperFirstLetter(s string) string {
 	return strings.ToUpper(string(s[0])) + s[1:]
 }
 
-func EncodeFieldname(k string) string {
-	if Iskey(k) {
-		return k + "_"
-	}
-	return k
-}
+//func EncodeFieldname(k string) string {
+//	if Iskey(k) {
+//		return k + "_"
+//	}
+//	return k
+//}
 
-func Iskey(name string) bool {
-	switch name {
-	case "break", "default", "func", "interface", "select", "case", "defer", "go", "map", "struct", "chan", "else", "goto", "package", "switch", "const", "fallthrough", "if", "range", "type", "continue", "for", "import", "return", "var":
-		return true
-	default:
-		return false
-	}
-}
+//func Iskey(name string) bool {
+//	switch name {
+//	case "break", "default", "func", "interface", "select", "case", "defer", "go", "map", "struct", "chan", "else", "goto", "package", "switch", "const", "fallthrough", "if", "range", "type", "continue", "for", "import", "return", "var":
+//		return true
+//	default:
+//		return false
+//	}
+//}
 
-func DecodeFieldname(name string) string {
-	if name[len(name)-1:] == "_" {
-		if n := name[:len(name)-1]; Iskey(n) {
-			return n
-		}
-	}
-	return name
-}
+//func DecodeFieldname(name string) string {
+//	if name[len(name)-1:] == "_" {
+//		if n := name[:len(name)-1]; Iskey(n) {
+//			return n
+//		}
+//	}
+//	return name
+//}
 
 func Recover(errp *error) {
 	if r := recover(); r != nil {
-		*errp = fmt.Errorf("panic recovering: %v", r)
+		if errp != nil {
+			*errp = fmt.Errorf("panic recovering: %v", r)
+		}
 	}
 }
 
