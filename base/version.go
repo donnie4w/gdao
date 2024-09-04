9f4288174956d9fa02ccfb75c8be3c3e6ca10cef
donnie4w <donnie4w@gmail.com>
2024-09-04 23:46:56 +0800

    Create version.go

---
+++ base/version.go
@@ -0,0 +1,10 @@
+// Copyright (c) 2024, donnie <donnie4w@gmail.com>
+// All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+//
+// github.com/donnie4w/gdao
+
+package base
+
+const VERSION = "1.2.0"
