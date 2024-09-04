8ca8450efd27340360e05be0532df0592fc2473f
donnie4w <donnie4w@gmail.com>
2024-09-04 23:47:45 +0800

    Update mapperstruct_test.go

--- gdaoMapper/mapperstruct_test.go
+++ gdaoMapper/mapperstruct_test.go
@@ -11,11 +11,12 @@ import (
 	"encoding/xml"
 	"fmt"
 	"os"
+	"strings"
 	"testing"
 )
 
 func Test_check(t *testing.T) {
-	file, err := os.Open("mappers.xml")
+	file, err := os.Open("mapper.xml")
 	if err != nil {
 		fmt.Printf("Error opening file: %v\n", err)
 		return
@@ -82,48 +83,115 @@ func Test_mappers(t *testing.T) {
 	}
 }
 
-func Test_mapper(t *testing.T) {
-	// Open the XML file
-	file, err := os.Open("mapper.xml")
-	if err != nil {
-		fmt.Printf("Error opening file: %v\n", err)
-		return
-	}
-	defer file.Close()
-
-	// Parse the XML file
-	var mapper mapper
-	decoder := xml.NewDecoder(file)
-	err = decoder.Decode(&mapper)
-	if err != nil {
-		fmt.Printf("Error decoding XML: %v\n", err)
-		return
-	}
-
-	// Print the parsed data
-	fmt.Printf("Namespace: %s\n", mapper.Namespace)
-	fmt.Println("Select statements:")
-	for _, s := range mapper.Selects {
-		fmt.Printf("  ID: %s, ResultType: %s, Query: %s\n", s.ID, s.ResultType, s.Query)
-	}
-
-	fmt.Println("Insert statements:")
-	for _, ins := range mapper.Inserts {
-		fmt.Printf("  ID: %s, ParameterType: %s, Query: %s\n", ins.ID, ins.ParameterType, ins.Query)
-	}
-
-	fmt.Println("Update statements:")
-	for _, upd := range mapper.Updates {
-		fmt.Printf("  ID: %s, ParameterType: %s, Query: %s\n", upd.ID, upd.ParameterType, upd.Query)
-	}
-
-	fmt.Println("Delete statements:")
-	for _, del := range mapper.Deletes {
-		fmt.Printf("  ID: %s, ParameterType: %s, Query: %s\n", del.ID, del.ParameterType, del.Query)
-	}
-}
+//func Test_mapper(t *testing.T) {
+//	// Open the XML file
+//	file, err := os.Open("mapper.xml")
+//	if err != nil {
+//		fmt.Printf("Error opening file: %v\n", err)
+//		return
+//	}
+//	defer file.Close()
+//
+//	// Parse the XML file
+//	var mapper mapper
+//	decoder := xml.NewDecoder(file)
+//	err = decoder.Decode(&mapper)
+//	if err != nil {
+//		fmt.Printf("Error decoding XML: %v\n", err)
+//		return
+//	}
+//
+//	// Print the parsed data
+//	fmt.Printf("Namespace: %s\n", mapper.Namespace)
+//	fmt.Println("Select statements:")
+//	for _, s := range mapper.Selects {
+//		fmt.Printf("  ID: %s, ResultType: %s, Query: %s\n", s.ID, s.ResultType, s.Query)
+//	}
+//
+//	fmt.Println("Insert statements:")
+//	for _, ins := range mapper.Inserts {
+//		fmt.Printf("  ID: %s, ParameterType: %s, Query: %s\n", ins.ID, ins.ParameterType, ins.Query)
+//	}
+//
+//	fmt.Println("Update statements:")
+//	for _, upd := range mapper.Updates {
+//		fmt.Printf("  ID: %s, ParameterType: %s, Query: %s\n", upd.ID, upd.ParameterType, upd.Query)
+//	}
+//
+//	fmt.Println("Delete statements:")
+//	for _, del := range mapper.Deletes {
+//		fmt.Printf("  ID: %s, ParameterType: %s, Query: %s\n", del.ID, del.ParameterType, del.Query)
+//	}
+//}
 
 func Test_parser(t *testing.T) {
 	mp := newMapperParser()
 	mp.parser("mappers.xml")
+}
+
+func Test_mapper2(t *testing.T) {
+	exampleXML := `
+	<mapper namespace="exampleMapper">
+		<select id="findUser" resultType="User">
+			SELECT * FROM users
+		</select>
+	</mapper>
+	`
+	var mapper mapper
+	decoder := xml.NewDecoder(strings.NewReader(exampleXML))
+	decoder.Decode(&mapper)
+	for _, selectStmt := range mapper.CrudNodes {
+		fmt.Println(selectStmt.XMLName.Local, ",", selectStmt.XMLName.Space)
+		fmt.Println(strings.TrimSpace(selectStmt.Query))
+		fmt.Println(len(selectStmt.Dynamics))
+	}
+}
+
+func Test_dynamic(t *testing.T) {
+	exampleXML := `
+	<mapper namespace="exampleMapper">
+		<select id="findUser" resultType="User">
+			SELECT * FROM users
+			<where>
+				<if test="isActive">
+					AND active = 1
+					<if test="isVerified">
+						AND verified = 1
+					</if>
+				</if>
+			</where>
+			<choose>
+				<when test="isAdmin">
+					AND role = 'admin'
+				</when>
+				<otherwise>
+					AND role = 'user'
+				</otherwise>
+			</choose>
+			<set>
+				<if test="name != null">
+					name = #{name},
+				</if>
+				<if test="age != null">
+					age = #{age}
+				</if>
+			</set>
+			<foreach collection="ids" item="id" open="AND id IN (" close=")" separator=",">
+				#{id}
+			</foreach>
+		</select>
+	</mapper>
+	`
+	var mapper mapper
+	decoder := xml.NewDecoder(strings.NewReader(exampleXML))
+	decoder.Decode(&mapper)
+
+	// 输出解析结果
+	fmt.Printf("Namespace: %s\n", mapper.Namespace)
+	for _, selectStmt := range mapper.CrudNodes {
+		fmt.Printf("Select ID: %s, ResultType: %s\n", selectStmt.ID, selectStmt.ResultType)
+		for _, part := range selectStmt.Dynamics {
+			fmt.Println("Query Part:", part)
+		}
+	}
 }
