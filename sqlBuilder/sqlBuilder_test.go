d641240c4b96678fad3b210dc55410dff37e18a4
donnie4w <donnie4w@gmail.com>
2024-09-05 11:20:31 +0800

    Create sqlBuilder_test.go

---
+++ sqlBuilder/sqlBuilder_test.go
@@ -0,0 +1,129 @@
+// Copyright (c) 2024, donnie <donnie4w@gmail.com>
+// All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+//
+// github.com/donnie4w/gdao
+
+package sqlBuilder
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_sqlBuilder_if(t *testing.T) {
+	context := map[string]any{
+		"username": "John Doe",
+		"age":      30,
+		"emails":   []any{"john@example.com", "doe@example.com"},
+	}
+	builder := NewSqlBuilder()
+	builder.Append("SELECT * FROM users where").
+		AppendIf("age>0", context, "age=?", context["age"]).
+		Append("ORDER BY id ASC")
+	fmt.Println(builder.GetSql())
+	fmt.Println(builder.GetParameters())
+}
+
+func Test_sqlBuilder_trim(t *testing.T) {
+	context := map[string]any{
+		"username": "John Doe",
+		"age":      30,
+		"emails":   []any{"john@example.com", "doe@example.com"},
+	}
+	builder := NewSqlBuilder()
+	builder.Append("SELECT * FROM users").
+		AppendTrim("WHERE ", "", "AND", "", func(trimBuilder *SqlBuilder) {
+			trimBuilder.AppendIf("username != nil", context, "AND username = ?", context["username"]).
+				AppendIf("age > 18", context, "AND age = ?", context["age"])
+		}).
+		Append("ORDER BY id ASC")
+	fmt.Println(builder.GetSql())
+	fmt.Println(builder.GetParameters())
+}
+
+func Test_sqlBuilder_choose(t *testing.T) {
+	context := map[string]any{
+		"username": "John Doe",
+		"age":      30,
+		"emails":   []any{"john@example.com", "doe@example.com"},
+	}
+	builder := NewSqlBuilder()
+	builder.Append("SELECT * FROM users where").
+		AppendChoose(context, func(chooseBuilder *ChooseBuilder) {
+			chooseBuilder.When("username != nil", "AND username = ?", context["username"]).
+				When("age > 0", "AND age = ?", context["username"]).
+				Otherwise("age=?", 1)
+		}).
+		Append("ORDER BY id ASC")
+	fmt.Println(builder.GetSql())
+	fmt.Println(builder.GetParameters())
+}
+
+func Test_sqlBuilder_foreach(t *testing.T) {
+	context := map[string]any{
+		"username": "John Doe",
+		"age":      30,
+		"emails":   []any{"john@example.com", "doe@example.com"},
+	}
+	builder := NewSqlBuilder()
+	builder.Append("SELECT * FROM users").
+		Append("where email in").
+		AppendForeach("emails", context, "email", ",", "(", ")", func(foreach *ForeachBuilder) {
+			foreach.Body("?")
+		}).
+		Append("ORDER BY id ASC")
+	fmt.Println(builder.GetSql())
+	fmt.Println(builder.GetParameters())
+}
+
+func Test_AppendSet(t *testing.T) {
+	context := map[string]any{
+		"username": "John Doe",
+		"age":      30,
+	}
+	builder := NewSqlBuilder()
+	builder.Append("UPDATE users").
+		AppendSet(func(setBuilder *SqlBuilder) {
+			setBuilder.AppendIf("username != nil", context, "username = ?,", context["username"]).
+				AppendIf("age == 30", context, "age = ?,", context["age"])
+		}).
+		Append("WHERE id = ?", 10)
+	fmt.Println(builder.GetSql())
+	fmt.Println(builder.GetParameters())
+}
+
+func Test_AppendTrim(t *testing.T) {
+	context := map[string]any{
+		"username": "John Doe",
+		"age":      30,
+	}
+	builder := NewSqlBuilder()
+	builder.Append("SELECT * FROM users").
+		AppendTrim("WHERE", "", "AND", "", func(trimBuilder *SqlBuilder) {
+			trimBuilder.AppendIf("username != nil", context, "AND username = ?", context["username"]).
+				AppendIf("age > 18 && age<30", context, "OR age = ?", context["age"])
+		}).
+		Append("OR id = ?", 10)
+
+	fmt.Println(builder.GetSql())
+	fmt.Println(builder.GetParameters())
+}
+
+func Test_Choose(t *testing.T) {
+	context := map[string]any{
+		"username": "John Doe",
+		"age":      30,
+	}
+	builder := NewSqlBuilder()
+	builder.Append("SELECT * FROM users where 1=1").AppendChoose(context, func(chooseBuilder *ChooseBuilder) {
+		chooseBuilder.When("age > 38", "AND age > ?", context["age"])
+		chooseBuilder.When("age > 19", "AND age >= ?", context["age"])
+		chooseBuilder.When("username != nil", "AND username = ?", context["username"])
+		chooseBuilder.When("username != nil || age>0 ", "AND username = ?", context["username"])
+		chooseBuilder.Otherwise("AND email IS NULL")
+	})
+	fmt.Println(builder.GetSql())
+	fmt.Println(builder.GetParameters())
+}
