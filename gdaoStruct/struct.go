4f10ef28695506b3b62b9994d8322d0a2d183a2a
donnie4w <donnie4w@gmail.com>
2024-09-05 11:20:16 +0800

    Create struct.go

---
+++ gdaoStruct/struct.go
@@ -0,0 +1,12 @@
+// Copyright (c) 2024, donnie <donnie4w@gmail.com>
+// All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+//
+// github.com/donnie4w/gdao
+
+package gdaoStruct
+
+type TableClass interface {
+	className() string
+}
